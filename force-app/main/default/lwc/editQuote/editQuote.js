a6affbdb41c2c0744e668f493552b4e4c515f501
Manish Singh Bhandari <manish.bhandari@metacube.com>
2023-02-23 12:20:47 +0530

    new fork done

--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -3,13 +3,25 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
-
+import { LightningElement, api, wire } from "lwc";
+import getQuoteData from '@salesforce/apex/QuoteController.getQuoteData';
+import { getRecord,getFieldValue} from 'lightning/uiRecordApi';
+/* https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.reference_lightning_ui_api_record */
 export default class EditQuote extends LightningElement {
   @api recordId;
-  quoteData = {
-    name: "Quote Name",
-    endDate: 1547250828000
+  contactData;
+  error;
+
+  @wire(getQuoteData)
+  quoteData({data, error}){
+    if(data){
+      console.log(data);
+      this.contactData = data;
+      this.error = undefined;
+    }else if(error){
+      this.contactData = undefined;
+      this.error = error;
+    }
   };
 
   renderedCallback() {}
